1a77495ff8fd967a3d50990734c097185a9eed3c
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-04-24 08:43:48 -0700

    add app js and css to components. Create file structure

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <p>header</p>
+    <p>home</p>
+    <p>list</p>
+    <p>checkout</p>
+    <p>footer</p>
+  );
+}
+
+export default App;
