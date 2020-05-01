791d9f5c0cacdfe83ffd5375e6a1d9f07d82407f
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-05-01 09:38:21 -0700

    add second test to add beer to list w/ data, FAIL

--- src/__tests__/reducers/beer-list-reducer.test.js
+++ src/__tests__/reducers/beer-list-reducer.test.js
@@ -14,4 +14,23 @@ describe('beerListReducer', () => {
     test('Should return default state if no action passed to reducer', () => {
         expect(beerListReducer({}, {type: null })).toEqual({});
     });
+
+    test('Should successfully add a new beer to masterBeerList', () => {
+        const { name, description, quantity, id } = beerData;
+        action = {
+            type: 'ADD_BEER',
+            name: name,
+            description: description,
+            quantity: quantity,
+            id: id
+        };
+        expect(beerListReducer({}, action)).toEqual({
+            [id]: {
+                name: name,
+                description: description,
+                quantity: quantity,
+                id: id
+            }
+        });
+    });
 });
