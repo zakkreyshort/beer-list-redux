f600d07cd116a9c8d3604e61c8d88d92532805bd
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-05-01 09:49:48 -0700

    Pass second test for adding beer to list

--- src/reducers/beer-list-reducer.js
+++ src/reducers/beer-list-reducer.js
@@ -1,3 +1,16 @@
 export default (state = {}, action) => {
-    return state;
+    switch (action.type){
+        case 'ADD_BEER':
+            const { name, description, quantity, id } = action;
+            return Object.assign({}, state, {
+                [id]: {
+                    name: name,
+                    description: description,
+                    quantity: quantity,
+                    id: id
+                }
+            });
+            default:
+                return state;
+    }
 };
