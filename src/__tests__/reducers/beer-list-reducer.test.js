d14e05d2459f43e07adc6eb8f24d95e61f60c945
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-05-01 09:25:56 -0700

    create beer data for beer list reducer

--- src/__tests__/reducers/beer-list-reducer.test.js
+++ src/__tests__/reducers/beer-list-reducer.test.js
@@ -1,6 +1,16 @@
 import beerListReducer from "../../reducers/beer-list-reducer";
 
 describe('beerListReducer', () => {
+
+    let action;
+    const beerData = {
+        name: 'ZakkTestIPA',
+        description: 'YummyAF',
+        quantity: 124,
+        id: 1
+    };
+
+
     test('Should return default state if no action passed to reducer', () => {
         expect(beerListReducer({}, {type: null })).toEqual({});
     });
