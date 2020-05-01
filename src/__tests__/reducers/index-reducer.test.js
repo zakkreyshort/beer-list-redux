8b42a719d6277c06f93ce2341999017726f5fdc3
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-05-01 11:26:07 -0700

    pass index reducer tests

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -15,10 +15,22 @@ describe("rootReducer", () => {
     });
 
     test('Check that initial state of beerListReducer matches root reducer', () => {
-        expect(store.getState().masterBeerList).toEqual(beerListReducer(undefined, {type: null}));
+        const action = {
+            type: 'ADD_BEER',
+            name: 'ZakkTestIPA',
+            description: 'YummyAF',
+            quantity: 124,
+            id: 1
+        }
+        store.dispatch(action);
+        expect(store.getState().masterBeerList).toEqual(beerListReducer(undefined, action));
     });
 
     test('Check that initial state of formVisibleReducer matches root reducer', () => {
-        expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, {type: null}));
+        const action = {
+            type: 'TOGGLE_FORM'
+        }
+        store.dispatch(action);
+        expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
     });
 });
